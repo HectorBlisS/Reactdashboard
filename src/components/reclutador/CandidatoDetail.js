96a8666ff48c4da4d53151515e990f5a25092df0
Oswaldinho <oswalfut_96@hotmail.com>
2017-09-22 17:52:04 -0500

    :smile: porfin

--- src/components/reclutador/CandidatoDetail.js
+++ src/components/reclutador/CandidatoDetail.js
@@ -1,12 +1,23 @@
 import React, {Component, PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
+import {GridList, GridTile, Tabs, Tab, List, ListItem, RaisedButton, Dialog, TextField, FloatingActionButton,SelectField,MenuItem, DatePicker, Toggle} from 'material-ui';
+import Mail from 'material-ui/svg-icons/communication/email';
+import Phone from 'material-ui/svg-icons/communication/phone';
+import Port from 'material-ui/svg-icons/action/work';
+import Ev from 'material-ui/svg-icons/action/event';
+import Fing from 'material-ui/svg-icons/action/fingerprint';
+import Loc from 'material-ui/svg-icons/communication/location-on';
+import moment from 'moment';
+import User from 'material-ui/svg-icons/action/account-circle';
+
 
 
 class CandidatoDetail extends Component {
 
     state = {
-      candidato:{}
+      candidato:{},
+        openAsesor:false,
     };
 
     componentWillReceiveProps(nextProps){
@@ -17,11 +28,101 @@ class CandidatoDetail extends Component {
     openEditForm = () => {
       this.props.setCandidato(this.state.candidato);
     };
+    //make asesor
+    handleOpenAsesor = () => {
+        this.setState({openAsesor: true});
+    };
+
+    handleCloseAsesor = () => {
+        this.setState({openAsesor: false});
+    };
+    handleTexto=(e)=>{
+        let field = e.target.name;
+        let candidato = this.state.candidato;
+        candidato[field] = e.target.value;
+        this.setState({candidato})
+    };
+    convertirAsesor=()=>{
+        if(window.confirm("Una vez que asignes estos datos, no podras cambiarlos")){
+            let asesor = Object.assign({}, this.state.candidato);
+            asesor['candidato'] = false;
+            this.props.saveCandidato(asesor);
+            this.handleCloseAsesor();
+        }
+
+    };
 
     render() {
         const {candidato, candidatos} = this.state;
         return (
-            <div style={{paddingTop:274}}>
+            <div style={{paddingTop:74}} className="candidato-tabla">
+                <GridList cols={3} cellHeight='auto'>
+                    <GridTile cols={1} className="candidato-datos">
+                        {candidato.candidato?<RaisedButton label="Convertir en Asesor" fullWidth={true} onTouchTap={this.handleOpenAsesor }/>:''}
+                        <Dialog
+                            title="Completa para acreditar como Asesor"
+                            modal={false}
+                            open={this.state.openAsesor}
+                            onRequestClose={this.handleCloseAsesor}>
+
+
+                            <GridList cellHeight={'auto'} cols={3}>
+                                <GridTile cols={1}>
+                                    <TextField
+                                        value={candidato.id_asesor}
+                                        hintText="Clave de Asesor"
+                                        floatingLabelText="Clave de Asesor"
+                                        name={"id_asesor"}
+                                        onChange={this.handleTexto}/>
+
+                                </GridTile>
+                                <GridTile cols={1}>
+                                    <TextField
+                                        value={candidato.tipo}
+                                        hintText="Tipo de Asesor"
+                                        floatingLabelText="Tipo de Asesor"
+                                        name={"tipo"}
+                                        onChange={this.handleTexto}/>
+
+                                </GridTile>
+                                <GridTile cols={1}>
+                                    <TextField
+                                        value={candidato.oficina}
+                                        hintText="Oficina"
+                                        floatingLabelText="Oficina"
+                                        name={"oficina"}
+                                        onChange={this.handleTexto}/>
+                                </GridTile>
+
+                                <RaisedButton label="Guardar" fullWidth={true} onTouchTap={this.convertirAsesor}/>
+                            </GridList>
+                        </Dialog>
+
+                        <List style={{textAlign:'left'}}>
+                            <ListItem disabled primaryText={candidato.nombre} leftIcon={<User/>} />
+                            <ListItem disabled primaryText={candidato.correo} leftIcon={<Mail/>} />
+                            <ListItem disabled primaryText={candidato.telefono} leftIcon={<Phone/>} />
+                            <ListItem disabled primaryText={moment(candidato.fecha_reclutamiento).format('LL')} leftIcon={<Ev/>} />
+                            <ListItem disabled primaryText={candidato.candidato?'Candidato':'Asesor'} leftIcon={<Port/>} />
+                            {candidato.candidato?'':
+                                <div>
+                                    <ListItem disabled primaryText={candidato.oficina} leftIcon={<Loc/>} />
+                                    <ListItem disabled primaryText={candidato.tipo} leftIcon={<Fing/>} />
+                                </div>}
+                        </List>
+                        <RaisedButton
+                            fullWidth={true}
+                            onClick={this.openEditForm}
+                            label='Editar'
+                        />
+
+                    </GridTile>
+                    <GridTile cols={2}>
+
+                    </GridTile>
+                </GridList>
+
+
                 <a
                     onClick={this.openEditForm}
                     href="#!">
